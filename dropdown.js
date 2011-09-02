952257523f577d2d5b192ef268f03969d1575d7e
Ben Williams <busybeewilliams@gmail.com>
2011-09-02 10:25:35 +0100

    initial commit of dropdown tests

---
+++ dropdown.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function () {
+	$('#searchbox').keyup(function(event) {
+		var search_text = $('#searchbox').val();
+		var rg = new RegExp(search_text,'i');
+		$('#nav li').each(function(){
+ 			if($.trim($(this).html()).search(rg) == -1) {
+ 				$(this).css('display', 'none');
+			}	
+			else {
+				$(this).parents().css('display', '');
+				$(this).css('display', '');
+				$(this).parent().parent().css('background-color', '#CE2661');
+			}
+		});
+	});
+});
+ 
+$('#search_clear').click(function() {
+	$('#searchbox').val('');	
+ 
+	$('#nav li').each(function(){
+		$(this).parents().css('display', '');
+		$(this).css('display', '');
+		$(this).css('background-color', '#ffffff');
+		$(this).parents().css('background-color', '#ffffff');
+	});
+});
